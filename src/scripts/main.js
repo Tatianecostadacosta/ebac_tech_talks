0562d4b1bed987f071813a13a33b07c6d0b1406d
Tatiane Costa <tatianecostadacosta@gmail.com>
2026-06-28 18:29:47 -0300

    feat: landing page aniversário 44 anos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,19 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
+const dataDoEvento = new Date("Jul 28, 2026 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
+const contador = document.getElementById('contador');
 
 const contaAsHoras = setInterval(function () {
-
     const agora = new Date();
     const timeStampAtual = agora.getTime();
-
     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        contador.innerHTML = '🎉 Feliz Aniversário!';
+        return;
+    }
 
     const diaEmMs = 1000 * 60 * 60 * 24;
     const horaEmMs = 1000 * 60 * 60;
@@ -19,12 +24,6 @@ const contaAsHoras = setInterval(function () {
     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
 
-    document.getElementById('contador').innerHTML =
-        `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
-
-        if (distanciaAteOEvento < 0) {
-            clearInterval(contaAsHoras);
-            document.getElementById('contador').innerHTML = 'Evento expirado'
-        }
+    contador.innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
 
 }, 1000);
